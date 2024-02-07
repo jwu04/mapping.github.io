bb61b70a33614c9272bd884488f2fa394c91efb0
Astronic <wu.jonathan09@gmail.com>
2024-02-07 16:59:26 -0500

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,71 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'The Mapping Manual',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Manual',
+        link: '/introduction/',
+      },
+    ],
+    sidebar: [
+      {
+        title: 'Introduction',
+        collapsable: false,
+        children: [
+          '/introduction/',
+          '/introduction/requirements'
+        ]
+      },
+      {
+        title: 'Week 01 - Concepts',
+        collapsable: false,
+        children: [ 
+          '/concepts/',
+          '/concepts/emphasis',
+          '/concepts/contrast',
+          '/concepts/consistency',
+          '/concepts/spread',
+
+        ],
+      }
+    ], 
+  },
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
